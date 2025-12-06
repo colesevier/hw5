aa0dc44868b578fc11c1d4a27ef5c10c7e4b4d16
Cole Sevier <sevier@usc.edu>
2025-12-06 08:31:11 +0000

    new adds and not pasing the dumb test

--- schedwork.cpp
+++ schedwork.cpp
@@ -7,6 +7,7 @@
 #include <map>
 #include <algorithm>
 // add or remove necessary headers as you please
+#include <functional>
 
 #endif
 
@@ -21,7 +22,9 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+// static bool backtrack(const AvailabilityMatrix &avail, size_t dailyNeed,
+//                     size_t maxShifts, DailySchedule &sched, vector<size_t> &shiftUsed,
+//                     size_t day, size_t slot);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,9 +40,53 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    size_t n = avail.size();
+    size_t k = avail[0].size();
+    // prepare schedule output
+    sched.assign(n, vector<Worker_T>(dailyNeed, INVALID_ID));
+    //track shifts
+    vector<size_t> shiftsUsed(k, 0);
+    function<bool(size_t, size_t)> solve = [&](size_t day, size_t slot) -> bool
+    {
+        if (day == n)
+        {
+            return true;
+        }
+        if (slot == dailyNeed)
+        {
+            return solve(day + 1, 0);
+        }
+        for (size_t w = 0; w < k; w ++)
+        {
+            // check availability
+            if (!avail[day][w])
+            {
+                continue;
+            }
+            if (shiftsUsed[w] >= maxShifts)
+            {
+                continue;
+            }
+            if (find(sched[day].begin(), sched[day].begin() + slot, w) != sched[day].begin() + slot)
+            {
+                continue;
+            }
+            // if we get to this point choose this worker
+            sched[day][slot] = w;
+            shiftsUsed[w] ++;
+            if (solve(day, slot + 1))
+            {
+                return true;
+            } 
 
-
-
-
+            // backtrack
+            shiftsUsed[w]--;
+            sched[day][slot] = INVALID_ID;
+        }
+        // return false otherwise
+        return false;
+    };
+    // call the program
+    return solve(0, 0);
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -14,6 +14,60 @@ using namespace std;
 
 // Add prototypes of helper functions here
 
+void build(string &current, int idx, map<char, int> &need, int remaining, 
+    const set<string> &dict, set<string> &out)
+{
+    int n = current.size();
+    if (idx == n)
+    {
+        if (remaining == 0 && dict.find(current) != dict.end())
+        {
+            out.insert(current);
+        }
+        return;
+    }
+    if (current[idx] != '-')
+    {
+        build(current, idx + 1, need, remaining, dict, out);
+        return;
+    }
+    int blanks = 0;
+    for (int i = idx; i < n; i++)
+    {
+        if (current[i] == '-') 
+        {
+            blanks++;
+        }
+    }
+
+    if (remaining > blanks) 
+        return;
+
+    for (auto &p : need) 
+    {
+        char c = p.first;
+        if (p.second > 0) 
+        {
+            current[idx] = c;
+            p.second--;
+            build(current, idx+1, need, remaining-1, dict, out);
+            p.second++;
+        }
+    }
+    
+    if (remaining < blanks)
+    {
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            if (need.find(c) == need.end() || need[c] == 0)
+            {
+                current[idx] = c;
+                build(current, idx + 1, need, remaining, dict, out);
+            }
+        }
+    }
+    current[idx] = '-';
+}
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +76,31 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    set<string> out;
+    string current = in;
+    map<char, int> need;
 
+    // find the number of each letter in floating
+    for (char c : floating)
+    {
+        need[c] ++;
+    }
+    int remaining = floating.size();
+    // am i allowed to use the auto iterator?
+    for (char c: in)
+    {
+        if (c != '-' && need[c] > 0)
+        {
+            need[c]--;
+            remaining--;
+
+        }
+    }
+    // call helper
+    build(current, 0, need, remaining, dict, out);
+    return out;
 }
 
 // Define any helper functions here
+// Don't I write the code above?
+
